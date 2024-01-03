2e3df5366202d90eadea43c3effcf9d1b505780f
Manjula36 <manjulavdt@gmail.com>
2024-01-03 17:59:48 +0530

    successfully committed

---
+++ StudentData/StudentData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData
+{
+    class Program
+    {
+        private static string filepath;
+
+        static void Main()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Studentdata.txt");
+
+            if (File.Exists(filePath))
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("file not found:" + filepath);
+            }
+        }
+    }
+}
+
